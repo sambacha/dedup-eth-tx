c086a584384ef0bb3494b268370143d383ad8fda
sam <sam@freighttrust.com>
2021-01-26 21:26:04 -0800

    fetch data script

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl https://api.blockcypher.com/v1/eth/main/txs/0x0c447d0398c29b5b360b80050e881047aa197694d4d4d49816d44c7c015178b8 > PPLNS.json
 curl https://api.blockcypher.com/v1/eth/main/blocks/6819126 > 6819126.json
+curl https://api.blockcypher.com/v1/eth/main/blocks/6819132 > 6819132.json
